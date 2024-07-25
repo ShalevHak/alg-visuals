59a69a4229436b73697112fc4d81019297326889
ShalevHak <shalev.haklai@gmail.com>
2024-07-25 23:24:40 +0300

    Implemented SearchTree class for representing bst Added drawTree for visualizing a bst
    Todo: add searching in bst visualization

--- SearchTree.js
+++ SearchTree.js
@@ -1,36 +1,111 @@
 class SearchTree{
-    /* #private */Val;
-    /* #private */Left;
-    /* #private */Right;
-    constructor(val,l=null,r=null){
-        this./* #private */Val=val;
-        this./* #private */Right=r;
-        this./* #private */Left=l;
+    // /* #private */val;
+    // /* #private */left;
+    // /* #private */right;
+    // parent;
+    constructor(val,layer=1,parent=null,l=null,r=null/* ,x,y */){
+        this./* #private */val=val;
+        this./* #private */right=r;
+        this./* #private */left=l;
+        this.parent=parent;
+        this.pos=[]
+        this.layer=layer;
+        this.index=null;
+
     }
-    Add=(val)=>{
-        if(val==this./* #private */Val)return;
-        if(val<this./* #private */Val){
-            if(this./* #private */Left!=null){
-                this./* #private */Left.Add(val);
-                return;
-            }
-            else{
-                this./* #private */Left=new SearchTree(val);
-                return;
-            }
-        }
-        if(val>this./* #private */Val){
-            if(this./* #private */Right!=null){
-                this./* #privateR */ight.Add(val);
-                return;
-            }
-            else{
-                this./* #private */Right=new SearchTree(val);
-                return;
+    bfsIndexing=()=>{
+        let q =[];
+        q.push(this);
+        while(q.length>0){
+            let count = q.length;
+            for(let i=0;i<count;i++){
+                let temp = q.shift();
+                temp.index=i;
+                if(temp.left!=null)q.push(temp.left);
+                if(temp.right!=null)q.push(temp.right);
+                str +=` ${temp.val} `;
             }
         }
     }
-    Remove = (parent)=>{
-        parent
+    add=(val)=>{
+        if(val==this./* #private */val)return;
+        if(val<this./* #private */val){
+            if(this./* #private */left!=null){
+                this./* #private */left.add(val);
+                return;
+            }
+            else{
+                this./* #private */left=new SearchTree(val,this.layer+1,this);
+                return;
+            }
+        }
+        if(val>this./* #private */val){
+            if(this./* #private */right!=null){
+                this./* #privateR */right.add(val);
+                return;
+            }
+            else{
+                this./* #private */right=new SearchTree(val,this.layer+1,this);
+                return;
+            }
+        }
+    }
+    getMinVal=()=>{
+        if(this==null)return null;
+        if(this.left==null&&this.right==null)return this.val;
+        if(this.left==null)return this.right.getMinVal();
+        return this.left.getMinVal();
+    }
+    search=(val)=>{
+        if(val==this./* #private */val)return this;
+        if(val<this./* #private */val){
+            if(this./* #private */left!=null){
+                return this./* #private */left.search(val);
+            }
+            return null;
+        }
+        if(val>this./* #private */val){
+            if(this./* #private */right!=null){
+                return this./* #privateR */right.search(val);
+            }
+            return null;
+        }
+    }
+    // remove = (val)=>{
+    //     let del = this.search(val);
+    //     if(this==null||del==null)return;
+    //     if(this.left!=null&&this.right!=null)return;
+    //     if(this.right!=null){
+    //         this.val=this.left.getMinVal();
+    //         this.left.remove(this.val);
+    //         return;
+    //     }
+    //     if(this.left!=null){
+    //         this.val=this.right.getMinVal();
+    //         this.right.remove(this.val);
+    //     }
+    // }
+    getHeight=()=> {
+        if (this === null) return 0;
+        if(this.left==null&&this.right==null) return 1;
+        if(this.left==null) return 1 + this.right.getHeight();
+        if(this.right==null) return 1 + this.left.getHeight();
+        return 1 + Math.max(this.left.getHeight(), this.right.getHeight());
+      }
+    toString =()=>{
+        let str="tree: \n";
+        let q =[];
+        q.push(this);
+        while(q.length>0){
+            let count = q.length;
+            for(let i=0;i<count;i++){
+                let temp = q.shift();
+                if(temp.left!=null)q.push(temp.left);
+                if(temp.right!=null)q.push(temp.right);
+                str +=` ${temp.val} `;
+            }
+            str+='\n';
+        }
+        return str;
     }
 }
--- main.js
+++ main.js
@@ -1,16 +1,138 @@
 let colors = [];
-let w = 500;
+let w = 1500;
 let h = 500;
 let cellSize;
-let fr = 20;
-
-
+let fr = 2;
+let [x,y]=[w/2,20];
+let t = null;
+let count =0;
+let curr=null;
+let searched;
+let searching;
 // Other functions like initializeGenerations(), findNextGen(), getRuleSet(), drawGen(), etc. should follow here
 
 function setup() {
     totalGens = Math.floor(h / cellSize);
     createCanvas(w, h);
     background(200);
-    frameRate(fr);
+    //frameRate(fr);
+    //console.log(t.search(7).toString())
+    //console.log(t.search(5).getMinVal())
     //colors = [color(255, 0, 0), color(0, 255, 0), color(0, 0, 255)]; // Example colors
+}
+function mouseClicked() {
+    let rnd = Math.round(random(100));
+    if(t==null){
+        t=new SearchTree(rnd);
+        drawTree(t);
+        return;
+    }
+    while(count<100&&t.search(rnd)!=null){
+        rnd = Math.round(random(100));
+    }
+    t.add(rnd);
+    drawTree(t);
+    count++;
+}
+function keyPressed(){
+    if (key === 's') {
+        curr = t;
+        if(curr!=null){
+            searched = Math.round(random(100));
+            searching=true;
+        }
+    }
+}
+function draw(){
+    if(searching){
+        drawSearchText();
+        //drawSearch(curr,searched);
+    }
+}
+function drawSearchText(){
+    fill(255,30,30);
+    rect(10,2,200,30);
+    fill(0,0,0);
+    textAlign(LEFT,CENTER);
+    text("Searching for: "+searched,20,10);
+}
+function drawSearch(root,n){
+    //Todo: add drawSearch
+    // background(200);
+    // if(n==root.val){
+    //     fill(255,30,30);
+    //     ellipse(root.pos[0],root.pos[1],30,20);
+    //     fill(0,0,0);
+    //     textAlign(CENTER,CENTER);
+    //     text(root.val,root.pos[0],root.pos[1]);
+    // }
+    // let q =[];
+    // q.push(t);
+    // let layer = 0;
+    // let h = t.getHeight();
+    // while(q.length>0){
+    //     let count = q.length;
+    //     for(let i=0;i<count;i++){
+    //         let temp = q.shift();
+    //         if(temp.parent!=null){
+    //             line(temp.pos[0],temp.pos[1],temp.parent.pos[0],temp.parent.pos[1]);
+    //         }
+            
+    //         if(temp.left!=null)q.push(temp.left);
+    //         if(temp.right!=null)q.push(temp.right);
+    //     }
+    //     layer++;
+    // }
+    // q.push(t);
+    // layer = 0;
+    // while(q.length>0){
+    //     let count = q.length;
+    //     for(let i=0;i<count;i++){
+    //         let temp = q.shift();
+    //         ellipse(temp.pos[0],temp.pos[1],30,20);
+    //         textAlign(CENTER,CENTER);
+    //         text(temp.val,temp.pos[0],temp.pos[1]);
+    //         if(temp.left!=null)q.push(temp.left);
+    //         if(temp.right!=null)q.push(temp.right);
+    //     }
+    //     layer++;
+    // }
+}
+function drawTree(t){
+    background(200);
+    let q =[];
+    q.push(t);
+    let layer = 0;
+    let h = t.getHeight();
+    while(q.length>0){
+        let count = q.length;
+        for(let i=0;i<count;i++){
+            let temp = q.shift();
+            temp.pos=[x+i*60-layer*20-h*15,y+layer*30]
+            if(temp.parent!=null){
+                line(temp.pos[0],temp.pos[1],temp.parent.pos[0],temp.parent.pos[1]);
+            }
+            
+            if(temp.left!=null)q.push(temp.left);
+            if(temp.right!=null)q.push(temp.right);
+        }
+        layer++;
+    }
+    q.push(t);
+    layer = 0;
+    while(q.length>0){
+        let count = q.length;
+        for(let i=0;i<count;i++){
+            let temp = q.shift();
+            fill(255,255,255);
+            ellipse(temp.pos[0],temp.pos[1],30,20);
+            fill(0,0,0);
+            textAlign(CENTER,CENTER);
+            text(temp.val,temp.pos[0],temp.pos[1]);
+            if(temp.left!=null)q.push(temp.left);
+            if(temp.right!=null)q.push(temp.right);
+        }
+        layer++;
+    }
+    console.log(t.toString());
 }
