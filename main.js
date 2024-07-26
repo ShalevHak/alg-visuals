2b183607ca2096c5883fda2821fadd208f7f8169
ShalevHak <shalev.haklai@gmail.com>
2024-07-26 15:07:07 +0300

    Added size var

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let colors = [];
-let w = 2500;
+let w = 1500;
 let h = 1500;
 let cellSize;
-let fr = 5;
+let fr = 2;
 let [x,y]=[w/2,20];
 let t = null;
 let count =0;
@@ -10,16 +10,12 @@ let curr=null;
 let searched;
 let searching;
 let str;
-// Other functions like initializeGenerations(), findNextGen(), getRuleSet(), drawGen(), etc. should follow here
+let size = 2;
 
 function setup() {
-    totalGens = Math.floor(h / cellSize);
     createCanvas(w, h);
     background(200);
     frameRate(fr);
-    //console.log(t.search(7).toString())
-    //console.log(t.search(5).getMinVal())
-    //colors = [color(255, 0, 0), color(0, 255, 0), color(0, 0, 255)]; // Example colors
 }
 document.getElementById("search_btn").addEventListener("click",searchOnClick);
 function searchOnClick(){
@@ -73,64 +69,33 @@ function drawSearch(root,n){
         return searched+" not found...";
     }
     if(n==root.val){
-        textSize(20)
+        textSize(20*size)
         fill(255,30,30);
-        ellipse(root.pos[0],root.pos[1],30,20);
+        ellipse(root.pos[0],root.pos[1],30*size,20*size);
         fill(0,0,0);
         textAlign(CENTER,CENTER);
         text(root.val,root.pos[0],root.pos[1]);
         return "Found "+ searched +"!";
     }
     else if(n<root.val){
-        textSize(10)
+        textSize(12*size)
         fill(0,50,255);
-        ellipse(root.pos[0]-25,root.pos[1],20,10);
+        ellipse(root.pos[0]-25*size,root.pos[1],20*size,10*size);
         fill(255,255,255);
         textAlign(CENTER,CENTER);
-        text(n,root.pos[0]-25,root.pos[1]);
+        text(n,root.pos[0]-25*size,root.pos[1]);
         curr=root.left;
     }
     else{
-        textSize(10)
+        textSize(12*size)
         fill(0,255,50);
-        ellipse(root.pos[0]+25,root.pos[1],20,10);
+        ellipse(root.pos[0]+25*size,root.pos[1],20*size,10*size);
         fill(255,255,255);
         textAlign(CENTER,CENTER);
-        text(n,root.pos[0]+25,root.pos[1]);
+        text(n,root.pos[0]+25*size,root.pos[1]);
         curr=root.right;
     }
     return "Searching for: "+searched;
-    // let q =[];
-    // q.push(t);
-    // let layer = 0;
-    // let h = t.getHeight();
-    // while(q.length>0){
-    //     let count = q.length;
-    //     for(let i=0;i<count;i++){
-    //         let temp = q.shift();
-    //         if(temp.parent!=null){
-    //             line(temp.pos[0],temp.pos[1],temp.parent.pos[0],temp.parent.pos[1]);
-    //         }
-            
-    //         if(temp.left!=null)q.push(temp.left);
-    //         if(temp.right!=null)q.push(temp.right);
-    //     }
-    //     layer++;
-    // }
-    // q.push(t);
-    // layer = 0;
-    // while(q.length>0){
-    //     let count = q.length;
-    //     for(let i=0;i<count;i++){
-    //         let temp = q.shift();
-    //         ellipse(temp.pos[0],temp.pos[1],30,20);
-    //         textAlign(CENTER,CENTER);
-    //         text(temp.val,temp.pos[0],temp.pos[1]);
-    //         if(temp.left!=null)q.push(temp.left);
-    //         if(temp.right!=null)q.push(temp.right);
-    //     }
-    //     layer++;
-    // }
 }
 function drawTree(t){
     background(200);
@@ -142,7 +107,7 @@ function drawTree(t){
         let count = q.length;
         for(let i=0;i<count;i++){
             let temp = q.shift();
-            temp.pos=[x+i*60-layer*20-h*15,y+layer*30]
+            temp.pos=[x+(i*60-layer*20-h*15)*size,y+layer*30*size]
             if(temp.parent!=null){
                 line(temp.pos[0],temp.pos[1],temp.parent.pos[0],temp.parent.pos[1]);
             }
@@ -159,9 +124,9 @@ function drawTree(t){
         for(let i=0;i<count;i++){
             let temp = q.shift();
             fill(255,255,255);
-            ellipse(temp.pos[0],temp.pos[1],30,20);
+            ellipse(temp.pos[0],temp.pos[1],30*size,20*size);
             fill(0,0,0);
-            textSize(20)
+            textSize(20*size)
             textAlign(CENTER,CENTER);
             text(temp.val,temp.pos[0],temp.pos[1]);
             if(temp.left!=null)q.push(temp.left);
